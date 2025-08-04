5f60d6f2f1eadd016a33948a50ac12413b413c1f
danhkhai07 <aliveomg@gmail.com>
2025-08-04 23:30:25 +0700

    revert back to prior testing card.h & card.cpp

--- include/data/dataHandler.h
+++ include/data/dataHandler.h
@@ -7,23 +7,6 @@
 
 class Data {
     public:
-        void test(){
-            Card card;
-
-            std::vector<std::string> testReviews = { "Good",
-                "Good", "Good", "Hard", "Good", "Easy", 
-                "Good", "Hard", "Again", "Good", "Easy", 
-                "Good", "Again", "Good", "Good", "Easy", 
-                "Hard", "Good", "Again", "Hard", "Easy" 
-            };
-
-            for (const auto& review : testReviews) {
-                std::cout << "Review result: " << review << "\n";
-                card.review(inputToGrade(review));
-            }
-
-            return;
-        }
     private:
         
 };
