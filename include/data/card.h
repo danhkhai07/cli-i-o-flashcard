4fc92a6ec47cacdd407885c3b877d55ce3962739
danhkhai07 <aliveomg@gmail.com>
2025-08-08 12:37:57 +0700

    new structure completed and tested

--- include/data/card.h
+++ include/data/card.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include "date/date.h"
+// #include "../utils/date/date.h" // pre production
+
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <functional>
 
 enum class Grade {Again, Hard, Good, Easy};
 enum class CardState {New, Learn, Review, Lapse};
@@ -10,21 +15,40 @@ Grade inputToGrade(const std::string& input);
 
 struct Card {
     public:
+        Card(){
+            calculateGradeTimeIntervals();
+        }
         std::string front, back;
-
-        CardState state = CardState::New;
-        double easeFactor = 2.5;
-        double interval = 0;
-        int currentStep = 0;
-        std::vector<double> learningStep = {1.0/1440, 10.0/1440, 15.0/1440};
+        std::unordered_map<Grade, double> gradeTimeIntervals;
+        std::string lastRefresh;
 
         void review(const Grade& grade);
+        bool due();
 
     private:
+        CardState state = CardState::New;
+        int currentStep = 0;
+        double easeFactor = 2.5;
+        double interval = 0;
+        std::vector<double> learningStep = {1.0/1440, 10.0/1440, 15.0/1440};
+        
+        std::function<double(Grade)> mult = [this](Grade grade) {
+            if (Grade::Hard == grade) return 1.2;
+            if (Grade::Good == grade) return easeFactor;
+            return easeFactor * 1.3; // Grade::Easy == grade
+        };
+        static const std::unordered_map<Grade, int> presetLearnStep;
+        static const std::unordered_map<Grade, int> presetLapseStep;
+
+        // Handlers
+        void learnHandler(const Grade& grade);
+        void reviewHandler(const Grade& grade);
+        void lapseHandler(const Grade& grade);
+
+        // Utilities
+        void calculateGradeTimeIntervals();
+        void printGradeTimeInt();
         void printNextReview();
-        double learnHandler(const Grade& grade);
-        double reviewHandler(const Grade& grade);
-        double lapseHandler(const Grade& grade);
         void adjustEaseFactor(const Grade& grade);
         void adjustInterval();
 };
--- src/data/card.cpp
+++ src/data/card.cpp
@@ -1,4 +1,5 @@
 #include "data/card.h"
+// #include "../../include/data/card.h" // pre production
 
 #include <unordered_map>
 #include <functional>
@@ -6,11 +7,29 @@
 #include <iostream>
 #include <iomanip>
 
+const std::unordered_map<Grade, int> Card::presetLearnStep = {
+    {Grade::Again, 0},
+    {Grade::Hard, 1},
+    {Grade::Good, 2}
+};
+
+const std::unordered_map<Grade, int> Card::presetLapseStep = {
+    {Grade::Again, 1},
+    {Grade::Hard, 2}
+};
+
 Grade inputToGrade(const std::string& input) {
     static std::unordered_map<std::string, Grade> gradeMap = {
         { "Again", Grade::Again }, { "Hard", Grade::Hard }, { "Good", Grade::Good }, { "Easy", Grade::Easy }
     };
     return gradeMap[input];
+}
+
+void Card::printGradeTimeInt(){
+    std::cout << "Again: " << (Card::gradeTimeIntervals[Grade::Again] < 1 ? Card::gradeTimeIntervals[Grade::Again] * 1440 : Card::gradeTimeIntervals[Grade::Again]);
+    std::cout << " | Hard: " << (Card::gradeTimeIntervals[Grade::Hard] < 1 ? Card::gradeTimeIntervals[Grade::Hard] * 1440 : Card::gradeTimeIntervals[Grade::Hard]);
+    std::cout << " | Good: " << (Card::gradeTimeIntervals[Grade::Good] < 1 ? Card::gradeTimeIntervals[Grade::Good] * 1440 : Card::gradeTimeIntervals[Grade::Good]);
+    std::cout << " | Easy: " << Card::gradeTimeIntervals[Grade::Easy] << "\n";
 }
 
 void Card::review(const Grade& grade){
@@ -20,92 +39,110 @@ void Card::review(const Grade& grade){
         {CardState::Review, [this](const Grade& grade){ reviewHandler(grade); } },
         {CardState::Lapse, [this](const Grade& grade){ lapseHandler(grade); } }
     };
+    printGradeTimeInt(); // pre production
     stateHandler[state](grade);
+    calculateGradeTimeIntervals();
     adjustInterval();
-    printNextReview();
+    printNextReview(); // pre production
 }
 
-void Card::printNextReview() {
+void Card::printNextReview(){
     if (interval < 1.0)
         std::cout << "Next review in " << interval * 1440 << " minutes\n";
     else
         std::cout << "Next review in " << interval << " days\n";
 }
 
-double Card::learnHandler(const Grade& grade){
-    static std::unordered_map<Grade, int> presetStep = {
-        {Grade::Again, 0},
-        {Grade::Hard, 1},
-        {Grade::Good, 2}
-    };
+void Card::calculateGradeTimeIntervals(){
+    switch (state){
+        case CardState::Learn:
+        case CardState::New:
+            // Again
+            gradeTimeIntervals[Grade::Again] = learningStep[presetLearnStep.at(Grade::Again)];
 
+            // Hard
+            gradeTimeIntervals[Grade::Hard] = learningStep[presetLearnStep.at(Grade::Hard)];
+
+            // Good
+            if (currentStep > 1) gradeTimeIntervals[Grade::Good] = 1;
+            else gradeTimeIntervals[Grade::Good] = learningStep[presetLearnStep.at(Grade::Good)];
+
+            // Easy
+            gradeTimeIntervals[Grade::Easy] = 2;
+            break;
+
+        case CardState::Review:
+            // Again
+            gradeTimeIntervals[Grade::Again] = learningStep[presetLapseStep.at(Grade::Again)];
+            
+            // Hard
+            gradeTimeIntervals[Grade::Hard] = interval * mult(Grade::Hard);
+
+            // Good
+            gradeTimeIntervals[Grade::Good] = interval * mult(Grade::Good);
+
+            // Easy
+            gradeTimeIntervals[Grade::Easy] = interval * mult(Grade::Easy);
+            break;
+
+        case CardState::Lapse:
+            // Again
+            gradeTimeIntervals[Grade::Again] = learningStep[presetLapseStep.at(Grade::Again)];
+
+            // Hard
+            gradeTimeIntervals[Grade::Hard] = learningStep[presetLapseStep.at(Grade::Hard)];
+
+            // Good
+            gradeTimeIntervals[Grade::Good] = 1;
+
+            // Easy
+            gradeTimeIntervals[Grade::Easy] = 4;
+            break;
+
+    }
+}
+
+void Card::learnHandler(const Grade& grade){
+    interval = gradeTimeIntervals[grade];
     switch (grade)
     {
         case Grade::Again:
-            interval = learningStep[presetStep[grade]];
             currentStep = 0;
             break;
         case Grade::Hard:
-            interval = learningStep[presetStep[grade]];
             currentStep = 1;
             break;
         case Grade::Good:
             if (currentStep > 1) {
-                interval = 1;
                 state = CardState::Review;
                 break;
             }
-            interval = learningStep[presetStep[grade]];
             currentStep = 2;
             break;
         case Grade::Easy:
             interval = 2;
             break;
     }
-    return interval;
 }
 
-double Card::reviewHandler(const Grade& grade){
+void Card::reviewHandler(const Grade& grade){
+    interval = gradeTimeIntervals[grade];
     if (grade == Grade::Again){
         state = CardState::Lapse;
-        currentStep = 1;
-        interval = learningStep[currentStep];
     } else {
-        auto mult = [this, grade](){
-            if (grade == Grade::Hard) return 1.2;
-            if (grade == Grade::Good) return easeFactor;
-            return easeFactor * 1.3; // grade == Grade::Easy
-        };
-        interval *= mult();
         adjustEaseFactor(grade);
     }
-    return interval;
 }
 
-double Card::lapseHandler(const Grade& grade){
-    static std::unordered_map<Grade, int> presetStep = {
-        {Grade::Again, 1},
-        {Grade::Hard, 2}
-    };
-
+void Card::lapseHandler(const Grade& grade){
+    interval = gradeTimeIntervals[grade];
     switch (grade)
     {
-        case Grade::Again:
-            interval = learningStep[presetStep[grade]];
-            break;
-        case Grade::Hard:
-            interval = learningStep[presetStep[grade]];
-            break;
         case Grade::Good:
-            interval = 1;
-            state = CardState::Review;
-            break;
         case Grade::Easy:
-            interval = 2;
             state = CardState::Review;
             break;
     }
-    return interval;
 }
 
 void Card::adjustEaseFactor(const Grade& grade){
