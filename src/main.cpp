4f39310b26747b18720afed796374d8d851d52e1
danhkhai07 <aliveomg@gmail.com>
2025-08-15 15:34:54 +0700

    add Ctrl+C handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include "cli/cli.h"
+  
+#include <csignal>
+#include <iostream>
+
+void handleSigint(int) {
+    std::cout << "\nCtrl+C detected. Exiting...\n";
+    std::cin.setstate(std::ios::failbit); // force all cin reads to fail
+    std::exit(0);
+}
 
 int main(int agrc, char* agrv[]){
+    // Handles Ctrl+C
+    std::signal(SIGINT, handleSigint);
+
     App app;
     app.run(agrc, agrv);
     return 0;
